335b50bd8dd4c88563b4d6e48ee982dd8b072d53
IT5706021621187 <5706021621187@fitm.kmutnb.ac.th>
2016-02-10 00:57:05 +0700

    ครั้งที่ 1-2

--- resume1.php
+++ resume1.php
@@ -2,11 +2,17 @@
 <head><title> แล้วแสดง </title>
 <meta charset="utf-8">
 </head>
-<body>
+<body background="ow6yxr.png">
     <center>
-
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
 
   <?php
+
 
     $firstname = $_GET['firstname'];
     $lastname = $_GET['lastname'];
@@ -18,26 +24,37 @@
     $phone = $_GET['phone'];
     $school = $_GET['school'];
     $nameschool = $_GET['nameschool'];
+    $nickname = $_GET['nickname'];
+
 
     echo "<p>";
-    echo "<b>ข้อมูลผู้ใช้ใส่มา</b> <br/>";
+    echo "<b>ข้อมูลผู้ใช้ใส่มา</b> <br/><br/>";
+
     echo "<b>Firstname : </b> $firstname <br/> "  ;
+
     echo "<b>Lastname :</b> $lastname <br/> " ;
+
+    echo "<b>Nickname :</b> $nickname <br/> " ;
+
     echo "<b>Sex :</b> $sex <br/> ";
+
     echo "<b>Birthdate :</b>  $bday  <br/>";
     if($age <= 100 && $age >= 1)
       echo "<b>Age : </b>  $age <br/>";
     else
       echo "Unsuccessful <br/>";
 
-    echo "<b>Address : </b> $address <br/> "  ;
+    echo "<b>Address : </b> $address <br/> ";
+
     if(strlen($phone) == 10)
       echo "<b> Phone : </b> $phone <br/>";
     else
       echo "Unsuccessful <br/>";
 
     echo "<b>Email : </b> $email <br/> "  ;
+
     echo "<b>Academy : </b> $nameschool<br/>";
+
 
    ?>
    </center>
